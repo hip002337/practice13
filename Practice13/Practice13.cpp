d22457ca547c5288e21a6fe0808e0145ac149949
hip002337 <h_kato@hip-pro.info>
2024-11-08 16:47:57 +0900

    comit

---
+++ Practice13/Practice13.cpp
@@ -0,0 +1,43 @@
+﻿
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define NUMMAX 100;
+
+
+int main()
+{
+    char han[3][7] = {"Big", "Small", "Bingo!"};
+    unsigned int num;
+    unsigned int ateru;
+    char text[20];
+
+    // 0～99の乱数設定
+    srand((unsigned int)time(NULL));
+    num = rand() % NUMMAX;
+    printf("%u\n", num);
+    while (1) {
+        // 入力値読み込み
+        printf("1-100の数値を入力してください");
+        scanf_s("%u", &ateru);
+        
+        // 比較
+        if (ateru > num) {
+            strcpy_s(text, han[0]);
+        }
+        else if (ateru < num) {
+            strcpy_s(text, han[1]);
+        }
+        else {
+            strcpy_s(text, han[2]);
+            break;
+        }
+        printf("%s\n", text);
+    }
+    printf("%s\n", text);
+    return 0;
+}
+
